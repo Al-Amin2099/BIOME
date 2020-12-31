79e8c26de56d26a1e498f4ffb3680134bbae8aec
Tony Amin <TonyAmin2520@gmail.com>
2020-12-30 16:53:42 -0800

    Fetching User Posts

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,6 +1,10 @@
 // making a call to fetch user, call user, and save posts
 import firebase from 'firebase'
-import {USER_STATE_CHANGE} from '../constants/index'
+import {USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE} from '../constants/index'
+
+import firebase from 'firebase'
+import {SnapshotViewIOSCOmponent} from 'react-native'
+require('firebase/firestore')
 
 // ------------------------------------------------------------------------------------
 
@@ -17,6 +21,30 @@ export function fetchUser() {
                 else{
                     console.log("Snapshot Does Not Exist")
                 }
+            })
+    })
+}
+
+export function fetchUserPosts() {
+    return((dispatch) => {
+        firebase.firestore()
+            .collection("Posts")
+            .doc(firebase.auth().currentUser.uid)
+            .collection("userPosts")
+            .orderBy("creation", "asc")
+            .get()
+            .then((snapshot) => {
+                let posts = snapshot.docs.map(doc => {
+                    // map function will iterate through all docs in the snapshot and build an array
+                    // of posts
+                    const data = doc.data();
+                    const dataID = doc.id;
+                    return {
+                        dataID, 
+                        ...data
+                    }
+                })
+                dispatch({type: USER_POSTS_STATE_CHANGE, posts})
             })
     })
 }
