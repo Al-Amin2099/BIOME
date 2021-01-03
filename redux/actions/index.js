48389c5e9bd9638fe7ba6e7b4ccc2bfdcab33036
Tony Amin <TonyAmin2520@gmail.com>
2021-01-02 22:52:36 -0800

    working on user feed

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,6 +1,6 @@
 // making a call to fetch user, call user, and save posts
 import firebase from 'firebase'
-import {USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE} from '../constants/index'
+import {USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE, USER_FOLLOWING_STATE_CHANGE, USERS_DATA_STATE_CHANGE, USERS_POSTS_STATE_CHANGE} from '../constants/index'
 import {SnapshotViewIOSCOmponent} from 'react-native'
 require('firebase/firestore')
 
@@ -17,7 +17,7 @@ export function fetchUser() {
                     dispatch({type: USER_STATE_CHANGE, currentUser: snapshot.data()})
                 }
                 else {
-                    console.log("Snapshot Does Not Exist")
+                    console.log("Does Not Exist")
                 }
             })
     })
@@ -55,7 +55,63 @@ export function fetchUserFollowing() {
                     const id = doc.id;
                     return id
                 })
-                dispatch({type: USER_FOLLOWING_STATE_CHANGE, following}) 
+                dispatch({type: USER_FOLLOWING_STATE_CHANGE, following});
+                for (let i = 0; y < following.length; i++){
+                    dispatch(fetchUsersData(followingA[i]));
+                }
+            })
+    })
+}
+
+export function fetchUsersData(uid) {
+    return((dispatch, getState) => {
+        const found = getState().usersState.users.some(el => el.uid=== uid);
+
+        if(!found) {
+            firebase.firestore()
+            .collection("Users")
+            .doc(uid)
+            .get()
+            .then((snapshot) => {
+                if(snapshot.exists){
+                    let user = snapshot.data();
+                    user.uid = snapshot.id;
+
+                    dispatch({type: USERS_DATA_STATE_CHANGE, user})
+                    dispatch(fetchUsersFollowingPosts(user.id));
+                }
+                else {
+                    console.log("Does Not Exist")
+                }
+            })
+        }
+
+    })
+}
+
+export function fetchUsersFollowingPosts(uid) {
+    return((dispatch, getState) => {
+        firebase.firestore()
+            .collection("Posts")
+            .doc(uid)
+            .collection("userPosts")
+            .orderBy("creation", "asc")
+            .get()
+            .then((snapshot) => {
+                // value of uid gets lost due to asynchronus the moment you reach the callback function
+                // this way gets the uid of the user even after the value changes after accessing the database
+                const uid = snapshot.query.EP.path.segments[1]
+                console.log({snapshot, uid})
+                const user = getState().usersState.users.find(el => el.uid === uid);
+
+                let posts = snapshot.docs.map(doc => {
+                    // map function will iterate through all docs in the snapshot and build an array
+                    // of posts
+                    const data = doc.data();
+                    const id = doc.id;
+                    return {id, ...data, user}
+                })
+                dispatch({type: USERS_POSTS_STATE_CHANGE, posts, uid}) 
             })
     })
 }
