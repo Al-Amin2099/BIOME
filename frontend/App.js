b5dc32680dc808b667f08eedb10a7de4009f971b
Tony Amin <TonyAmin2520@gmail.com>
2021-01-24 19:22:42 -0800

    updated app icons

--- frontend/App.js
+++ frontend/App.js
@@ -124,24 +124,4 @@ export class App extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#4f6d7a'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  }
-
-})
-
 export default App
